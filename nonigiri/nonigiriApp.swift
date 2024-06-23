50adb92ebdde434b05a6e81139fe009710017f39
gracetownland <speak2ayushsrihari@gmail.com>
2024-06-22 22:02:23 -0700

    Initial Commit

---
+++ nonigiri/nonigiriApp.swift
@@ -0,0 +1,17 @@
+//
+//  nonigiriApp.swift
+//  nonigiri
+//
+//  Created by Ayush S on 2024-06-22.
+//
+
+import SwiftUI
+
+@main
+struct nonigiriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

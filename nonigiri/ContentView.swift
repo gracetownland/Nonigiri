7a833681007e410cca01552737710f904f1a731c
gracetownland <speak2ayushsrihari@gmail.com>
2024-06-22 23:02:10 -0700

    added basic ui elements. slider needs to be changed. timer functionality to be added.

--- nonigiri/ContentView.swift
+++ nonigiri/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var statusItem: NSStatusItem!
+    @State private var speed = 5.0
+    @State private var isEditing = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Slider(
+                        value: $speed,
+                        in: 0...120,
+                        onEditingChanged: { editing in
+                            isEditing = editing
+                        }
+                    )
+//                    Text("\(speed)")
+//                        .foregroundColor(isEditing ? .red : .blue)
+            //Text("SLIDER")
+            HStack{
+                Button("5m") {
+                    speed = 5
+                }
+                .buttonStyle(.accessoryBar)
+                .padding(5)
+                Button("10m") {
+                    speed = 10
+                }
+                .buttonStyle(.accessoryBar)
+                .padding(5)
+                Button("25m") {
+                    speed = 25
+                }
+                .buttonStyle(.accessoryBar)
+                .padding(5)
+                
+                
+                Spacer()
+                Image(systemName: "ellipsis")
+                    .padding()
+            }
+            HStack{
+                Button("Start"){
+                    speed+=speed
+                }
+                .buttonStyle(.accessoryBar)
+                .padding(10)
+    
+                Spacer()
+                Text("\(Int(speed))").bold().font(.system(size: 30))
+            }
         }
-        .padding()
     }
 }
 

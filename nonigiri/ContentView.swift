517d5e81c2ecd824ad6a504c3190ccd03212ab86
gracetownland <speak2ayushsrihari@gmail.com>
2024-06-23 00:48:05 -0700

    timer implementation added. buggy. counting is not periodic.

--- nonigiri/ContentView.swift
+++ nonigiri/ContentView.swift
@@ -7,18 +7,39 @@
 
 import SwiftUI
 import ModernSlider
+import Foundation
 
 struct ContentView: View {
     @State private var setTime = 5.0
     @State private var isEditing = false
     let mainScreen = NSScreen.main
-    
+    @State var isTimerRunning = false
+    func startTimer(){
+        isTimerRunning = true
+        var timerCount = 0
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                                timerCount += 1
+            if (timerCount == 10){
+                setTime = setTime - 0.01
+                timerCount = 0
+                
+                if(setTime <= 0.0)
+                {
+                    timer.invalidate()
+                }
+            }
+            
+                    }
+        //make timer functionality
+    }
     var body: some View {
+
         VStack{
+            
             ModernSlider(
                 "Time",
                 systemImage: "deskclock",
-                sliderWidth: 200,
+                sliderWidth: 300,
                 sliderHeight: 30,
                 sliderColor: .white,
                 value: Binding(
@@ -26,22 +47,27 @@ struct ContentView: View {
                     set: {setTime = $0 * 1.2}
                     )
                 )
+            .padding(.top)
             
 
             HStack{
                 Button("5m") {
                     setTime = 5
+                    startTimer()
                     //start timer
                 }
                 .buttonStyle(.accessoryBar)
                 .padding(5)
                 Button("10m") {
                     setTime = 10
+                    startTimer()
+                    //startTimer
                 }
                 .buttonStyle(.accessoryBar)
                 .padding(5)
                 Button("25m") {
                     setTime = 25
+                    startTimer()
                 }
                 .buttonStyle(.accessoryBar)
                 .padding(5)
@@ -52,15 +78,23 @@ struct ContentView: View {
                     .padding()
             }
             HStack{
-                Button("Start"){
-                    setTime = (setTime < 120) ? setTime + setTime : 120
+                Button("start"){
+                    //setTime = (setTime < 120) ? setTime + setTime : 120
+                    startTimer()
                 }
                 .buttonStyle(.accessoryBar)
                 .padding(10)
     
                 Spacer()
-                Text("\(Int(setTime))").bold().font(.system(size: 30))
+                Text(formattedtime).font(.system(size: 40)).fontDesign(.rounded)
+                    .padding(10)
             }
+        }
+        var formattedtime : String {
+            let minutes = Int(setTime)
+            let seconds = Int((setTime - Double(minutes)) * 60)
+            
+            return String(format: "%02d:%02d", minutes, seconds)
         }
     }
 }
